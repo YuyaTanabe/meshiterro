144f28bb7e514e063e08a9b0f1bb6833ff7fdb74
YuyaTanabe <gkumanokawa@gmail.comgit config --global user.email gkumanokawa@gmail.comgit config --global user.email gkumanokawa@gmail.com>
2018-11-18 07:18:36 +0000

    [Finish] 5-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  # /deviseのコントローラのデフォルトでは用意されていないカラム(name)を追加したための修正
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  # deviseのコントローラのデフォルトでは用意されていないカラム(name)を追加したための修正/
+
 end
